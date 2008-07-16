818dbe4e31c7fba28a23b50a5018a4f62d9f753a
Marcel Haerry <haerry@puzzle.ch>
2008-07-16 12:19:47 +0200

    fixed paths for java

--- files/profile.d/java.sh
+++ files/profile.d/java.sh
@@ -1,6 +1,2 @@
-export JAVA_HOME=/usr/local/java
-export CLASSPATH=$JAVA_HOME/jre/lib/rt.jar:
-export PATH=$JAVA_HOME/bin:$PATH
-# Xmx = 1/4 of physical memory, Xms = 1/8 of physical memory 
-export JAVA_OPTIONS="-Djava.awt.headless=true -Xms50m -Xmx100m"
-
+export JAVA_HOME=/usr/lib/jvm/jre
+export CLASSPATH=$JAVA_HOME/lib/rt.jar:
