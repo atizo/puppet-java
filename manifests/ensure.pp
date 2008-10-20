18312ee5339dc6053f5fef1050595fb492c9ae5f
Marcel Haerry <haerry@puzzle.ch>
2008-10-20 15:10:34 +0200

    added dependencies

--- manifests/ensure.pp
+++ manifests/ensure.pp
@@ -6,9 +6,11 @@ class java::ensure {
     exec{'ensure_java':
         command => 'alternatives --set java /usr/lib/jvm/jre-1.6.0-sun/bin/java',
         unless => "alternatives --display java | grep 'points to' | grep -q '/usr/lib/jvm/jre-1.6.0-sun/bin/java'",
+        require => [ Package['java-1.6.0-sun'], Package['java-1.6.0-sun-devel'], Package['java-1.6.0-sun-fonts'] ],
     }
     exec{'ensure_javac':
         command => 'alternatives --set javac /usr/lib/jvm/java-1.6.0-sun/bin/javac',
         unless => "alternatives --display javac| grep 'points to' | grep -q '/usr/lib/jvm/java-1.6.0-sun/bin/javac'",
+        require => [ Package['java-1.6.0-sun'], Package['java-1.6.0-sun-devel'], Package['java-1.6.0-sun-fonts'] ],
     }
 }
